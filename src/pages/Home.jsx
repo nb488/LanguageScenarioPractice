c9f8d6dc94e91062a3e6a0f2317a7b87a16cd18a
Natalia Blanco <121647379+nb488@users.noreply.github.com>
2025-10-12 00:22:44 -0700

    Initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to PlotTwist!</h1>
+            <p>Create and practice language learning using storytelling.</p>
+            <u>
+                <li>Login</li>
+                <li>Sign Up</li>
+            </u>
+        </div>
+    )
+}
